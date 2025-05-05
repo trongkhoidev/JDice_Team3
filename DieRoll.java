b5cd7c3f8d8382b8beca155f29e4aa55aea8a6ec
dtngochan <dtngochan0604@gmail.com>
2025-05-05 21:50:48 +0700

    Refactor DieRoll class: rename variables, add Javadoc comments
    Add input validation and logging to DieRoll class

--- DieRoll.java
+++ DieRoll.java
@@ -1,58 +1,59 @@
-import java.util.*;
-/*
-JDice: Java Dice Rolling Program
-Copyright (C) 2006 Andrew D. Hilton  (adhilton@cis.upenn.edu)
+import java.util.Random;
+import java.util.logging.Logger;
 
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
+/**
+ * Represents a die roll operation with specified number of dice, sides per die, and a bonus value.
+ * Provides functionality to roll dice and get a result.
  */
-
-
 public class DieRoll {
-    private int ndice;
-    private int nsides;
-    private int bonus;
-    private static Random rnd;
-    static{
-	rnd=new Random();
-    }
-    public DieRoll(int ndice,
-		   int nsides,
-		   int bonus) {
-	this.ndice=ndice;
-	this.nsides=nsides;
-	this.bonus=bonus;
-    }
-    public RollResult makeRoll() {
-	RollResult r=new RollResult(bonus);
-	for(int i=0;i<ndice;i++) {
-	    int roll=rnd.nextInt(nsides)+1;
-	    r.addResult(roll);
-	}
-	return r;
-    }
-    public String toString() {
-	String ans =ndice+"d"+nsides;
-	if(bonus>0) {
-	    ans= ans+"+"+bonus;
-	}
-	else if(bonus<0) {
-	    ans=ans+bonus;
-	}
-	return ans;
+    private final int ndice;
+    private final int nsides;
+    private final int bonus;
+    private static final Random rnd = new Random();
+    private static final Logger logger = Logger.getLogger(DieRoll.class.getName());
+
+    /**
+     * Constructs a DieRoll instance with validation.
+     * @param ndice Number of dice to roll (must be > 0)
+     * @param nsides Number of sides per die (must be > 0)
+     * @param bonus Additional bonus to add to result
+     * @throws IllegalArgumentException if ndice or nsides <= 0
+     */
+    public DieRoll(int ndice, int nsides, int bonus) {
+        if (ndice <= 0 || nsides <= 0) {
+            throw new IllegalArgumentException("Number of dice and sides must be > 0");
+        }
+        this.ndice = ndice;
+        this.nsides = nsides;
+        this.bonus = bonus;
     }
 
+    /**
+     * Rolls the dice and returns the result.
+     * @return RollResult object containing individual rolls and bonus
+     */
+    public RollResult makeRoll() {
+        RollResult result = new RollResult(bonus);
+        for (int i = 0; i < ndice; i++) {
+            int roll = rnd.nextInt(nsides) + 1;
+            result.addResult(roll);
+            logger.info("Rolled: " + roll);
+        }
+        return result;
+    }
+
+    /**
+     * Returns a string representation in format XdY±Z.
+     * @return Description of the roll h
+     */
+    @Override
+    public String toString() {
+        String description = ndice + "d" + nsides;
+        if (bonus > 0) {
+            description += "+" + bonus;
+        } else if (bonus < 0) {
+            description += bonus;
+        }
+        return description;
+    }
 }
